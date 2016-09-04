5fa842e5e8c5e9e478a94f13ee4d3393e6d88e65
Gabriel Gomez <leandrogrotnig11@gmail.com>
2016-09-04 01:36:04 -0300

    Pagina inicial de la web

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PROYECTO: Aula online</title>
+  </head>
+  <style>
+    *{
+      text-align: center;
+      font-family: verdana;
+      color:#3d3d3d;
+    }
+    span{
+      font-size: 18pt;
+    }
+  </style>
+  <body>
+    <h1>Seleccionar version</h1>
+    <hr><br>
+    <a href="beta"><span>Aulaonline_beta</span></a><br>[NO CSS]
+    <br><br>
+    <a href="release"><span>Aulaonline_release</span></a>
+  </body>
+</html>
